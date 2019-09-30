d9ca5366c4bd2f167a7fd299e7c79ee1c50cd9ca
pradyumn25 <45122170+pradyumn25@users.noreply.github.com>
2019-09-30 14:17:05 +0530

    Update BASIC_FORM.php

--- BASIC_FORM.php
+++ BASIC_FORM.php
@@ -5,29 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
 <?php $n_error=$r_error="";
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     
-if(empty($_POST['name']) && empty($_POST['roll']))
+if(empty($_POST['name']) or empty($_POST['roll']))
     {
         $n_error='enter name'."<br>";
         $r_error='enter roll';
     } 
 }?>
-
-    <form action="" method="POST">
-        name<input type="text" name="name">
-        <span style="color:red" >*  <?php echo $n_error;?></span><br>
-        roll<input type="number" name="roll">
-        <span style="color:red" >* <?php echo $r_error;?></span><br>
-        <input type="submit">
-    </form>
+    <div class="card" style="width: 18rem;">
+        <img src="wallpaper.jpg" class="card-img-top">
+        <div class="card-body">
+            <form action="" method="POST">
+                name<input type="text" name="name">
+                <span style="color:red" >*  <?php echo $n_error;?></span><br>
+                roll<input type="number" name="roll">
+                <span style="color:red" >* <?php echo $r_error;?></span><br>
+                <input type="submit">
+            </form>
+        </div>
+      </div>
+    
 <?php
    
-if (isset($_POST['name']) && isset($_POST['roll']))
+if (!empty($_POST['name']) && !empty($_POST['roll']))
     {
         echo $_POST['name']."<br>";
         echo $_POST['roll']."<br>";
